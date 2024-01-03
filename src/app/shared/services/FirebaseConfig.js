b473027e817eba69c42896fae6f0beff44947403
ViniMagaa <vinisantos2008vs@gmail.com>
2024-01-03 15:50:49 -0300

    adicionando firebase ao projeto

---
+++ src/app/shared/services/FirebaseConfig.js
@@ -0,0 +1,15 @@
+import * as firebase from "firebase/app";
+import * as firebaseAuth from "firebase/auth";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyA6HizYhUSowQYvmPQBHCxQWKCncuPjBw0",
+	authDomain: "piatto-divino.firebaseapp.com",
+	projectId: "piatto-divino",
+	storageBucket: "piatto-divino.appspot.com",
+	messagingSenderId: "788778410830",
+	appId: "1:788778410830:web:38e29982bfc397bb16b41f",
+	measurementId: "G-GNE9PFY8P1",
+};
+const app = firebase.initializeApp(firebaseConfig);
+
+export const auth = firebaseAuth.initializeAuth(app);
