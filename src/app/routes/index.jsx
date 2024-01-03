7d4b9878f162b363dff888309a04dcdfb49dfe48
ViniMagaa <vinisantos2008vs@gmail.com>
2024-01-03 19:55:23 -0300

    adicionando proteção às rotas privadas

--- src/app/routes/index.jsx
+++ src/app/routes/index.jsx
@@ -1,21 +1,31 @@
+import { useContext } from "react";
 import { Route, Routes } from "react-router-dom";
 
 import { About, Home, Login, Panel, Recipe, Recipes, Register } from "../pages";
 import { CreateRecipe, EditRecipe } from "../pages/chef";
+import RecipesContext from "../shared/contexts/RecipesContext";
 
 export const AppRoutes = () => {
+	const { isConnected } = useContext(RecipesContext);
+
 	return (
 		<Routes>
 			<Route path="/" element={<Home />} />
 			<Route path="/receitas" element={<Recipes />} />
 			<Route path="/receitas/:id" element={<Recipe />} />
 			<Route path="/sobre" element={<About />} />
-			<Route path="/cadastrar" element={<Register />} />
-			<Route path="/entrar" element={<Login />} />
 			<Route path="/chef" element={<Panel />} />
-			<Route path="/chef/editar" element={<Panel />} />
-			<Route path="/chef/editar/:id" element={<EditRecipe />} />
-			<Route path="/chef/criar" element={<CreateRecipe />} />
+			{!isConnected ? (
+				<>
+					<Route path="/cadastrar" element={<Register />} />
+					<Route path="/entrar" element={<Login />} />
+				</>
+			) : (
+				<>
+					<Route path="/chef/editar/:id" element={<EditRecipe />} />
+					<Route path="/chef/criar" element={<CreateRecipe />} />
+				</>
+			)}
 		</Routes>
 	);
 };
