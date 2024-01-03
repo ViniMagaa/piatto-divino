469b23f7df0ef2ad8937374f316531466c1383fa
ViniMagaa <vinisantos2008vs@gmail.com>
2024-01-03 15:50:49 -0300

    adicionando firebase ao projeto

--- src/index.js
+++ src/index.js
@@ -2,6 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app/App";
 import "./index.css";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyA6HizYhUSowQYvmPQBHCxQWKCncuPjBw0",
+	authDomain: "piatto-divino.firebaseapp.com",
+	projectId: "piatto-divino",
+	storageBucket: "piatto-divino.appspot.com",
+	messagingSenderId: "788778410830",
+	appId: "1:788778410830:web:38e29982bfc397bb16b41f",
+	measurementId: "G-GNE9PFY8P1",
+};
+initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
