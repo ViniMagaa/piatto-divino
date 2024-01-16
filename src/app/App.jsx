7b7e01ae4a8f7af22fe9462fb543da7c047f5391
ViniMagaa <vinisantos2008vs@gmail.com>
2024-01-16 12:11:24 -0300

    atualizando scroll somente quando houver mudança no pathname

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,16 +1,28 @@
-import { BrowserRouter } from "react-router-dom";
+import { useEffect } from 'react';
+import { BrowserRouter, useLocation } from "react-router-dom";
 
 import { Header, Main, Footer } from "./shared/components/layout";
 import { AppContextProvider } from "./shared/contexts";
 
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({top: 0, left: 0, behavior: "smooth"});
+  }, [pathname]);
+
+  return null;
+};
+
 export default function App() {
 	return (
-		<BrowserRouter>
-			<AppContextProvider>
-				<Header />
-				<Main />
-				<Footer />
-			</AppContextProvider>
-		</BrowserRouter>
-	);
+    <BrowserRouter>
+      <ScrollToTop />
+      <AppContextProvider>
+        <Header />
+        <Main />
+        <Footer />
+      </AppContextProvider>
+    </BrowserRouter>
+  );
 }
--- src/app/shared/components/Button/Button.jsx
+++ src/app/shared/components/Button/Button.jsx
@@ -1,18 +1,13 @@
 import "./Button.css";
 
 export const Button = ({ children, type, handleClick, customClassName }) => {
-	const handleOnClick = () => {
-		handleClick();
-		window.scrollTo({ top: 0, behavior: "smooth" });
-	};
-
-	return (
-		<button
-			type={type || "button"}
-			onClick={handleOnClick}
-			className={customClassName}
-		>
-			{children}
-		</button>
-	);
+  return (
+    <button
+      type={type || "button"}
+      onClick={handleClick}
+      className={`btn ${customClassName}`}
+    >
+      {children}
+    </button>
+  );
 };
